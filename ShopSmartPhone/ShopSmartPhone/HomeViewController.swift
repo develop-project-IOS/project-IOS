add8a9771c1deca7e98bf87b35f397e4357325c3
Huỳnh Duy Quốc <43953469+01297462246@users.noreply.github.com>
2020-07-18 06:55:23 -0700

    upload project first time

---
+++ ShopSmartPhone/ShopSmartPhone/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  ShopSmartPhone
+//
+//  Created by Huynh Duy Quoc on 7/11/20.
+//  Copyright © 2020 Huynh Duy Quoc. All rights reserved.
+//
+
+import UIKit
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ShopSmartPhone/ShopSmartPhone/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ShopSmartPhone
+//
+//  Created by Huynh Duy Quoc on 7/10/20.
+//  Copyright © 2020 Huynh Duy Quoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//    @IBAction func didTapButton(){
+//        let vc = UIViewController()
+//        navigationController?.pushViewController(vc, animated: true)
+//    }
+}
+
