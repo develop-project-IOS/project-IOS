c2851dfa8ddc97379497463d9dc987113f1f9eb3
QuocTo <nhoxkenl090@gmail.com>
2020-08-09 13:23:54 -0700

    Cart function
    Update BuyStoryboard
    Update BuyViewController design success

---
+++ ShopSmartPhone/ShopSmartPhone/BuyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BuyViewController.swift
+//  ShopSmartPhone
+//
+//  Created by Huynh Duy Quoc on 8/9/20.
+//  Copyright © 2020 Huynh Duy Quoc. All rights reserved.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
